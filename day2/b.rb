e7150d1e7cd59edf6869f5ab0680150bbcfcecb6
Jesse Falconer <jesse.falconer@spark.re>
2024-12-03 05:26:04 +0000

    Do not loop prematurely with excessive combos

--- day2/b.rb
+++ day2/b.rb
@@ -1,19 +1,19 @@
 count = 0
 
-def row_combinations(row)
-  row.each_with_index.map do |_, index|
-    row[0...index] + row[index + 1..-1]
-  end.unshift(row)
+def valid_row?(row)
+  slope = row.sort == row || row.sort.reverse == row
+  tolerance = row.each_cons(2).all? { (_1 - _2).abs <= 3 && !(_1 - _2).zero? }
+
+  slope && tolerance
 end
 
 File.foreach("day2/input.txt") do |line|
   row = line.split.map(&:to_i)
 
-  count += 1 if row_combinations(row).any? do |row|
-    slope = row.sort == row || row.sort.reverse == row
-    tolerance = row.each_cons(2).all? { (_1 - _2).abs <= 3 && !(_1 - _2).zero? }
+  count += 1 if row.each_with_index.any? do |_, index|
+    row_combination = row[0...index] + row[index + 1..-1]
 
-    slope && tolerance
+    valid_row?(row_combination)
   end
 end
 
