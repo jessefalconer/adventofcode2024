6ebee3cbfdfd5de77bd529063df5da15403de4d7
Jesse Falconer <jesse.falconer@spark.re>
2024-12-08 00:32:12 +0000

    Day 7 solutions

---
+++ day7/a.rb
@@ -0,0 +1,34 @@
+OPERATORS = %i[+ *]
+
+sum = 0
+max_permutation_size = 0
+
+equations = File.readlines("day7/input.txt", chomp: true).map do |line|
+  answer, operands = line.split(":")
+  operands = operands.strip.split(" ").map(&:to_i)
+  max_permutation_size = [max_permutation_size, operands.size - 1].max
+
+  [answer.to_i, operands]
+end
+permutation_hash = {}
+
+(1..max_permutation_size).each do |size|
+  permutation_hash[size] = OPERATORS.repeated_permutation(size).to_a
+end
+
+equations.each do |answer, operands|
+  permutation_hash[operands.size - 1].each do |permutation|
+    equation = operands.zip(permutation).flatten.compact
+    result = equation[0]
+
+    (1..equation.size - 1).step(2) do |index|
+      operator = equation[index]
+      operand = equation[index + 1]
+      result = result.send(operator, operand)
+    end
+
+    break sum += result if result == answer
+  end
+end
+
+puts sum
